2ceecab00a40e27d7cf8a5af3b1cf1fd36fa5cf4
vicentefiorito <vicentefiorito@gmail.com>
2022-05-26 00:06:29 -0400

    added more styling

--- screens/character.js
+++ screens/character.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
 
 function Character({ route }) {
   //
@@ -24,14 +25,17 @@ function Character({ route }) {
 
   if (char !== null) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.charName}>{char.name} </Text>
-        <Text>{char.gender}</Text>
-        <Text>{char.location.name}</Text>
-        <Image source={{uri: char.image}} style={styles.logo}/>
-        <Text>Species:{char.species}</Text>
-        <Text>Status: {char.status}</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+        <ImageBackground source={{ uri: char.image }} style={styles.img} resizeMode="cover" />
+          <Text style={styles.charName}>{char.name} </Text>
+          <Text style={styles.info}>General Info:</Text>
+          <Text>Gender: {char.gender}</Text>
+          <Text>Favorite Place: {char.location.name}</Text>
+          <Text>Species:{char.species}</Text>
+          <Text>Status: {char.status}</Text>
+        </View>
+      </ScrollView>
     );
   } else {
     return (
@@ -50,21 +54,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
   },
-  charName: {
-      flex: 1,
-      textAlign:"left",
-      color: "violet",
-      marginTop: 32,
-      paddingVertical: 8,
-      fontSize:32
 
-
+  info: {
+    flex:2,
+    color: "cyan",
+    fontSize: 32,
   },
-  logo: {
-    flex: 5,
-    width: "135%",
-    height:"60%",
-    alignItems:"flex-end",
-    paddingLeft: 8
+
+  charName: {
+    flex: 1,
+    textAlign: "left",
+    width: "100%",
+    color: "violet",
+    marginTop: 32,
+    paddingVertical: 8,
+    fontSize: 48,
+  },
+  img: {
+   flex:1,
+   justifyContent:"center"
   },
 });
