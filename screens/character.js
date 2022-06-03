a3b2764333af90c881e742a43dad38c9f041046b
vicentefiorito <vicentefiorito@gmail.com>
2022-06-02 23:20:21 -0400

    Fixed char bugs

--- screens/character.js
+++ screens/character.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground, Alert, Modal, TouchableOpacity } from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import * as Speech from "expo-speech";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import React from "react";
 
 function Character({ route }) {
   //
   const { data } = route.params;
   const [char, setChar] = useState(null);
-  console.log(data);
+  const [modalVisible,setModalVisible] = useState(false);
   // console.log(navigation);
 
   useEffect(() => {
@@ -23,23 +27,75 @@ function Character({ route }) {
   }
 
   if (char !== null) {
+    //console.log("hereee -->", char);
     return (
-      
-        <View style={styles.container}>
-        <ImageBackground source={{ uri: char.image }} style={styles.img} resizeMode="cover">
-          <Text style={styles.charName}>{char.name} </Text>
-          <Text style={styles.info}>General Info:</Text>
-          <Text style={styles.misc}>Gender: {char.gender}</Text>
-          <Text style={styles.misc}>Favorite Place: {char.location.name}</Text>
-          <Text style={styles.misc}>Species:{char.species}</Text>
-          <Text>Status: {char.status}</Text>
-          </ImageBackground>
-        </View>
+      <View style={styles.container}>
+        <ImageBackground
+          source={{ uri: char.image }}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <LinearGradient
+            // Background Linear Gradient
+            colors={["rgba(0,0,0,0.7)", "rgba(0,0,0,0.5 )"]}
+            style={styles.background}
+          >
+            <Text style={styles.title}>{char.name}</Text>
+
+            <View style={styles.row}>
+              <Text style={styles.textRight}>{char.gender}</Text>
+              <FontAwesome name="intersex" size={32} color="#2185d0" />
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.textRight}>{char.location.name}</Text>
+              <Ionicons name="home" size={32} color="#2185d0" />
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.textRight}>{char.species}</Text>
+              <Ionicons name="man" size={32} color="#2185d0" />
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.textRight}>{char.status}</Text>
+              <Ionicons name="md-checkmark-circle" size={32} color="green" />
+            </View>
+            {/* <Text style={styles.textRight}>{char.episode[0]}</Text> */}
+            {/* Has Additional Api */}
+
+            <TouchableOpacity
+              style={styles.buttonRow}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.rowText}>View Episodes</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Hello World!</Text>
+                <TouchableOpacity
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </ImageBackground>
+      </View>
     );
   } else {
     return (
       <View>
-        <Text>Not Found...</Text>
+        <Text>Loading...</Text>
       </View>
     );
   }
@@ -50,30 +106,115 @@ export default Character;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
   },
-
-  info: {
-    flex:1,
-    color: "cyan",
-    fontSize: 32,
-  },
-
-  charName: {
+  image: {
     flex: 1,
-    textAlign: "left",
+    justifyContent: "center",
+  },
+  background: {
+    flex: 1,
+  },
+  title: {
+    color: "white",
+    fontSize: 42,
     width: "100%",
-    color: "violet",
-    marginTop: 32,
+    // lineHeight: 84,
+    padding: 8,
+    fontWeight: "bold",
+    textAlign: "left",
+    // borderColor: "red",
+    // borderWidth: 1,
+    backgroundColor: "transparent",
+  },
+  textRight: {
+    color: "white",
+    fontSize: 24,
+    // width: "100%",
+    padding: 8,
+    textAlign: "right",
+    // borderBottomColor: "red",
+    // borderBottomWidth: 1,
+    backgroundColor: "transparent",
+  },
+  textLeft: {
+    color: "white",
+    fontSize: 24,
+    // width: "100%",
+    padding: 8,
+    textAlign: "left",
+    borderColor: "red",
+    borderWidth: 1,
+    backgroundColor: "transparent",
+  },
+  row: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    alignSelf: "center",
+    backgroundColor: "transparent",
+    margin: 8,
     paddingVertical: 8,
-    fontSize: 48,
+    borderRadius: 16,
   },
-  img: {
-   flex:1,
-   justifyContent:"center",
+  buttonRow: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    backgroundColor: "#2185d0",
+    margin: 8,
+    padding: 8,
+    borderRadius: 16,
   },
-  misc: {
-    color: 'white',
-  }
+  rowText: {
+    color: "white",
+    fontSize: 24,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    flex:1,
+    margin: 16,
+    width:'90%',
+    alignSelf:'center',
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 16,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
 });
+
--- screens/home.js
+++ screens/home.js
@@ -1,88 +1,90 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    SafeAreaView,
-    Button,
-    TouchableOpacity,
-  } from "react-native";
-  import { useEffect, useState, useRef } from "react";
-  import Card from "../components/Card";
-  
-  const Home = ({ navigation }) => {
-    // https://rickandmortyapi.com/api/character/?page=1
-    //
-    const [data, setData] = useState(null);
-    const counter = useRef(1);
-    //
-    useEffect(() => {
-      reset();
-    }, []);
-    //
-    async function getData() {
-      const req = await fetch(
-        `https://rickandmortyapi.com/api/character/?page=${counter.current}`
-      );
-      const res = await req.json();
-      // console.log(res)
-      await setData(res.results);
-      counter.current++;
-    }
-    //
-    async function reset() {
-      counter.current = 1;
-      getData();
-    }
-    //console.log(navigation);
-    if (data !== null) {
-      return (
-        <SafeAreaView style={styles.container}>
-          <Text style={{ fontSize: 32, fontStyle: "italic", color: "#2185d0" }}>
-            Rick And Morty
-          </Text>
-          <View style={{ marginTop: 16 }}>
-            <Button onPress={getData} title="Next Page" color="#841584" />
-          </View>
-          <View style={{ marginTop: 16, marginBottom: -16, paddingBottom: 16 }}>
-            <Button onPress={reset} title="Reset" color="#841584" />
-          </View>
-          <ScrollView
-            contentContainerStyle={{
-              // flex: 1,
-              minHeight: 900,
-              paddingVertical: 24,
-            }}
-          >
-            {data.map((item, index) => {
-              return (
-                <TouchableOpacity
-                  key={index}
-                  onPress={() => { 
-                    navigation.navigate("Character", {
-                      data: item,
-                    });
-                  }}
-                >
-                  <Card data={item} />
-                </TouchableOpacity>
-              );
-            })}
-          </ScrollView>
-        </SafeAreaView>
-      );
-    } else {
-      return <Text>Loading...</Text>;
-    }
-  };
-  
-  export default Home;
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useState, useRef } from "react";
+import Card from "../components/Card";
+
+const Home = ({ navigation }) => {
+  // https://rickandmortyapi.com/api/character/?page=1
+  //
+  const [data, setData] = useState(null);
+  const counter = useRef(1);
+  //
+  useEffect(() => {
+    reset();
+  }, []);
+  //
+  async function getData() {
+    const req = await fetch(
+      `https://rickandmortyapi.com/api/character/?page=${counter.current}`
+    );
+    const res = await req.json();
+    // console.log(res)
+    await setData(res.results);
+    counter.current++;
+  }
+  //
+  async function reset() {
+    counter.current = 1;
+    getData();
+  }
+  //
+  // console.log(navigation)
+  if (data !== null) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={{ fontSize: 32, fontStyle: "italic", color: "#2185d0" }}>
+          Rick And Morty
+        </Text>
+        <View style={{ marginTop: 16 }}>
+          <Button onPress={getData} title="Next Page" color="#841584" />
+        </View>
+        <View style={{ marginTop: 16, marginBottom: -16, paddingBottom: 16 }}>
+          <Button onPress={reset} title="Reset" color="#841584" />
+        </View>
+        <ScrollView
+          contentContainerStyle={{
+            // flex: 1,
+            minHeight: 900,
+            paddingVertical: 24,
+          }}
+        >
+          {data.map((item, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => { 
+                  navigation.navigate("Character", {
+                    data: item,
+                  });
+                }}
+              >
+                <Card data={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </SafeAreaView>
+    );
+  } else {
+    return <Text>Loading...</Text>;
+  }
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
